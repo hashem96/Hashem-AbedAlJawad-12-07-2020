72948dbd13bcd27dd7369f1168c82985d45d6e9b
Hashem Abed Al Jawad <dashinghashem@yahoo.com>
2020-07-15 16:03:40 +0300

    Fix mobile view of landing page

--- messaging-system/src/Components/button/button.js
+++ messaging-system/src/Components/button/button.js
@@ -3,7 +3,7 @@ import './button.css'
 
 const button = (props) => {
     return (
-        <div style = {{'marginTop': props.marginTop , 'marginLeft' : props.marginLeft}} className="btnContainer">
+        <div style = {{'marginTop': props.marginTop , 'marginLeft' : props.marginLeft}} className="buttonsContainer">
 
             <button id="btn" type={props.type}  style={{ 'background': props.color , 'width':props.width }}
                 onClick={props.onClick}> {props.text} </button>
